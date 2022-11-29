89ae5060536d18f1ebbf39560e24b2e193a4e5c7
Timur <golubev_t@mail.ru>
2022-11-29 23:36:25 +0300

    commit6

---
+++ task1/task1.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число a");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число b");
+int b = int.Parse(Console.ReadLine());
+if (a > b)
+{
+    Console.WriteLine(a);
+}
+else
+{
+    Console.WriteLine(b);
+}
