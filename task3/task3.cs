ba9f1727ce1f81b73040749ce1a9c48c482b6edd
Timur <golubev_t@mail.ru>
2022-12-01 22:50:08 +0300

    Решена задача 3

---
+++ task3/task3.cs
@@ -0,0 +1,5 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine());
+if (number % 2 == 0) Console.WriteLine($"{number}-Da");
+else Console.WriteLine($"{number}-NET");
